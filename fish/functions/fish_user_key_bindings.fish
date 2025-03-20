7122b02f3fbdc64897f651d68e58b6218265b606
André Melo <andregabrielmelo@gmail.com>
2025-03-20 10:58:17 -0300

    Delete fzf functions

--- fish/functions/fish_user_key_bindings.fish
+++
@@ -1,9 +0,0 @@
-function fish_user_key_bindings
-  # fzf
-  bind \cd fzf_change_directory_from_current_one
-  bind \cf fzf_change_directory
-  bind \cr _fzf_search_history
-end
-
-# fzf plugin
-fzf_configure_bindings --directory=\co
